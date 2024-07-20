554526b72d7c5619ec576bb7c6e42b07b94b9f1a
Vikram Saraph <vsaraph@tungstencarbide.ax3000.online>
2024-07-19 20:13:24 -0400

    dockerized ui

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <v-app>
     <v-main>
-      <HelloWorld />
+      <RecipeList />
     </v-main>
   </v-app>
 </template>
 
 <script setup lang="ts">
+import RecipeList from './components/RecipeList.vue';
+
   //
 </script>
